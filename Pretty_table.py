1b2d499331294b2be3c36977993b84618c39481a
Naresh Gudala <nareshgudala2002@gmail.com>
2025-09-09 23:21:09 +0530

    Update Pretty_table.py

--- Pretty_table.py
+++ Pretty_table.py
@@ -17,4 +17,16 @@ table.add_row([503,"Nikil",95])
 table.add_row([508,"Nimesh",100])
 table.add_row([510,"Naresh",93])
 
+
 print(table)
+
+output:
++--------+--------+-------+
+| Rollno |  Name  | Marks |
++--------+--------+-------+
+|  501   | Varma  |   89  |
+|  502   | Ganesh |   92  |
+|  503   | Nikil  |   95  |
+|  508   | Nimesh |  100  |
+|  510   | Naresh |   93  |
++--------+--------+-------+
